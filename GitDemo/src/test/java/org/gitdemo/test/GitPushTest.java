f7c2c965d50f20bfad498b3ece27c2fb47dd6438
91905 <91905@sailakshmi>
2023-10-08 18:31:05 +0530

    added some more changes

--- GitDemo/src/test/java/org/gitdemo/test/GitPushTest.java
+++ GitDemo/src/test/java/org/gitdemo/test/GitPushTest.java
@@ -5,6 +5,7 @@ public class GitPushTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Git push testing");
+		System.out.println("the last push was successful.");
 	}
 
 }
